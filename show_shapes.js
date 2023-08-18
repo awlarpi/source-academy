37bec060f19363b0fee63867e2cbb2b4b9fbe3cc
awlarpi <No public email provided>
2023-08-18 11:15:22 +0800

    Changes made from Source Academy

---
+++ show_shapes.js
@@ -0,0 +1,72 @@
+// import shapes
+import {
+    blank,
+    circle,
+    corner,
+    heart,
+    nova,
+    pentagram,
+    rcross,
+    ribbon,
+    sail,
+    square,
+    triangle
+} from "rune";
+
+// import colours
+import {
+    black,
+    blue,
+    brown,
+    color,
+    green,
+    indigo,
+    orange,
+    pink,
+    purple,
+    random_color,
+    red,
+    white,
+    yellow
+} from "rune";
+
+// import other functions
+import {
+    anaglyph,
+    animate_anaglyph,
+    animate_rune,
+    beside,
+    beside_frac,
+    flip_horiz,
+    flip_vert,
+    from_url,
+    hollusion,
+    hollusion_magnitude,
+    make_cross,
+    overlay,
+    overlay_frac,
+    quarter_turn_left,
+    quarter_turn_right,
+    repeat_pattern,
+    rotate,
+    scale,
+    scale_independent,
+    show,
+    stack,
+    stack_frac,
+    stackn,
+    translate,
+    turn_upside_down
+} from "rune";
+
+show(blank);
+show(circle);
+show(corner);
+show(heart);
+show(nova);
+show(pentagram);
+show(rcross);
+show(ribbon);
+show(sail);
+show(square);
+show(triangle);
