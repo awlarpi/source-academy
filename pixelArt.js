9cd90d2746dd47358017371b3f85ea048d7bdfb3
awlarpi <No public email provided>
2023-09-01 01:51:30 +0800

    Changes made from Source Academy

--- pixelArt.js
+++ pixelArt.js
@@ -54,47 +54,13 @@ import {
     turn_upside_down
 } from "rune";
 
+const size = 37;
+const shape = square;
+
 const arr = (v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37) => (f) => f(v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37);
 const first = (p) => p((v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37) => v1);
 const discard = (p) =>
     p((v1, v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37) => (f) => f(v2, v3, v4, v5, v6, v7, v8, v9, v10, v11, v12, v13, v14, v15, v16, v17, v18, v19, v20, v21, v22, v23, v24, v25, v26, v27, v28, v29, v30, v31, v32, v33, v34, v35, v36, v37, false));
-
-const size = 37;
-const shape = make_cross(square);
-
-const getShape = (v) => v === 1 ? shape : blank;
-
-const addRune = (n, row, val) => 
-    beside_frac((n - 1) / n, row, getShape(val));
-    
-const stackRow = (rune, nRows, newRow) => 
-    stack_frac((nRows - 1) / nRows, rune, newRow);
-
-function makeRow(vArr) {
-    function makeRowIter(vArr, accum, count) {
-        return count > size
-            ? accum
-            : makeRowIter(
-                    discard(vArr), 
-                    addRune(count, accum, first(vArr)), 
-                    count + 1
-                );
-    }
-    return makeRowIter(vArr, getShape(first(vArr)), 1);
-}
-    
-function makeArt(rvArr) {
-    function makeArtIter(rvArr, accum, count) {
-        return count > size
-            ? accum
-            : makeArtIter(
-                    discard(rvArr), 
-                    stackRow(accum, count, makeRow(first(rvArr))),
-                    count + 1
-                );
-    }
-    return makeArtIter(rvArr, makeRow(first(rvArr)), 1);
-}
 
 const imgData = arr(
     arr(1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1),
@@ -136,4 +102,38 @@ const imgData = arr(
     arr(1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 0, 0, 1)
 );
 
-show(makeArt(imgData));
+const getShape = (v) => v === 1 ? shape : blank;
+
+const addRune = (n, row, val) => 
+    beside_frac((n - 1) / n, row, getShape(val));
+    
+const stackRow = (rune, nRows, newRow) => 
+    stack_frac((nRows - 1) / nRows, rune, newRow);
+
+function makeRow(vArr) {
+    function makeRowIter(vArr, accum, count) {
+        return count > size
+            ? accum
+            : makeRowIter(
+                    discard(vArr), 
+                    addRune(count, accum, first(vArr)), 
+                    count + 1
+                );
+    }
+    return makeRowIter(vArr, getShape(first(vArr)), 1);
+}
+
+function runes_contest() {
+    function makeArtIter(rvArr, accum, count) {
+        return count > size
+            ? accum
+            : makeArtIter(
+                    discard(rvArr), 
+                    stackRow(accum, count, makeRow(first(rvArr))),
+                    count + 1
+                );
+    }
+    return makeArtIter(imgData, makeRow(first(imgData)), 1);
+}
+
+show(runes_contest());
